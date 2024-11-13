01fc06c0c2b964c12846ab9746b282898df638aa
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2024-11-12 16:03:43 -0800

    Update assets_to_ago.py

--- assets_to_ago.py
+++ assets_to_ago.py
@@ -9,31 +9,83 @@ import warnings
 warnings.simplefilter(action='ignore')
 
 import os
+import logging
 import psycopg2
+from psycopg2 import OperationalError 
+from psycopg2 import DatabaseError
 import pandas as pd
 import geopandas as gpd
-from shapely import wkb
+#from shapely import wkb
 
 import timeit
-start_t = timeit.default_timer() #start time
 
-# Database connection details
-host = os.getenv('PG_HOST_CW')
-port = os.getenv('PG_PORT_CW')
-database = os.getenv('PG_DATABASE_CW')
-user = os.getenv('PG_USER_CW')
-password = os.getenv('PG_PASSWORD_CW')
 
-try:
-    # Connect to PostgreSQL database
-    conn = psycopg2.connect(
-        host=host,
-        port=port,
-        database=database,
-        user=user,
-        password=password
-    )
 
+class PostgresDBManager:
+    def __init__(self, dbname, user, password, host="localhost", port="5432"):
+        """
+        Initializes the PostgresDBConnector with database connection parameters.
+        """
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        """Establishes a connection to the PostgreSQL database."""
+        try:
+            self.connection = psycopg2.connect(
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port
+            )
+            logging.info("..connection established successfully.")
+            return self.connection
+        
+        except OperationalError as e:
+            logging.error(f"..error connecting to database: {e}")
+            self.connection = None
+
+    def disconnect(self):
+        """Closes the connection to the PostgreSQL database."""
+        if self.connection:
+            try:
+                self.connection.close()
+                logging.info("..connection closed.")
+                
+            except DatabaseError as e:
+                logging.error(f"..error closing connection: {e}")
+                
+            finally:
+                self.connection = None
+                self.cursor = None
+        else:
+            logging.warning("..no active database connection to close.")
+
+    def create_cursor(self):
+        """Creates a cursor object for executing queries."""
+        if self.connection:
+            try:
+                self.cursor = self.connection.cursor()
+                logging.info("..cursor created successfully.")
+                
+            except DatabaseError as e:
+                logging.error(f"..error creating cursor: {e}")
+                self.cursor = None
+                
+        else:
+            logging.warning("..no active  connection.")
+
+
+def read_assets(conn):
+    """
+    Returns a dataframe containing assets point data 
+    """
     # Fetch tables and schema names
     sqlTabs= """
         SELECT table_name
@@ -49,10 +101,10 @@ try:
     ]
 
     # Read tables
-    tab_dict= {}
+    assets_dict= {}
     for table_name in tab_names:
-        print (f'\nreading table: {table_name}')
-        if table_name in ['trails', 'roads']:
+        print (f'..reading table: {table_name}')
+        if table_name in ['trails', 'roads']: #centroids
             query= f"""
                 SELECT 
                     *, 
@@ -94,13 +146,34 @@ try:
                     
         df = pd.read_sql(query, conn)
         df.drop(columns=['wkb_geometry'], inplace=True)
-        tab_dict[table_name]= df
+        assets_dict[table_name]= df
         
+        #concatinate tables data into a signle df
+        df = pd.concat(
+            assets_dict.values(), 
+            ignore_index=True
+        )
 
+    return df
+
+
+def read_trails(conn):
+    """
+    Returns a geodataframe containing trails line data 
+    """
+    query = "SELECT * FROM assets.trails"
+    gdf = gpd.read_postgis(
+        query, 
+        conn, 
+        geom_col='wkb_geometry'
+    )
     
-    # Concatenate all DataFrames into one
-    df_all = pd.concat(tab_dict.values(), ignore_index=True)
+    return gdf
 
+def process_assets (df, latcol, loncol):
+    """
+    Returns a gdf of Cleans-up and transforms Assets data
+    """
     # Filter asset categories
     cats=[
     'Grounds',
@@ -118,38 +191,65 @@ try:
     'Water Management',
     'Fuel Storage',
     'Tools']
-    df_all= df_all[df['asset_category'].isin(cats)]
+    
+    df= df[df['asset_category'].isin(cats)]
 
     # Keep only points within BC
     lat_min, lat_max = 47, 60
     lon_min, lon_max = -145, -113
 
-    df_all = df_all[
-        (df_all['gis_latitude'] >= lat_min) & (df_all['gis_latitude'] <= lat_max) &
-        (df_all['gis_longitude'] >= lon_min) & (df_all['gis_longitude'] <= lon_max)
+    df = df[
+        (df[latcol] >= lat_min) & (df[latcol] <= lat_max) &
+        (df[loncol] >= lon_min) & (df[loncol] <= lon_max)
     ]
     
+    gdf = gpd.GeoDataFrame(
+        df,
+        geometry=gpd.points_from_xy(df[loncol], df[latcol]),
+        crs="EPSG:4326"
+    )
+    
+    return gdf
 
-
-    # Read trails table into a gdf
-    print ('\nreading table: trails')
-    query_t = "SELECT * FROM assets.trails"
-    #df_trl= pd.read_sql(query, conn) 
-    gdf_trl = gpd.read_postgis(query_t, conn, geom_col='wkb_geometry')
+if __name__ == "__main__":
+    start_t = timeit.default_timer() #start time
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    
+    try:
+        logging.info("Connecting to CityWide database")
+        pg= PostgresDBManager(
+            host= os.getenv('PG_HOST_CW'),
+            port= os.getenv('PG_PORT_CW'),
+            dbname= os.getenv('PG_DATABASE_CW'),
+            user= os.getenv('PG_USER_CW'),
+            password= os.getenv('PG_PASSWORD_CW')
+        )
+        conn= pg.connect()
         
-
-except Exception as e:
-    print(f"An error occurred: {e}")
-
-finally:
-    # Close the connection
-    if conn:
-        conn.close()
-
-
-
-finish_t = timeit.default_timer() #finish time
-t_sec = round(finish_t-start_t)
-mins = int (t_sec/60)
-secs = int (t_sec%60)
-print('\nProcessing Completed in {} minutes and {} seconds'.format (mins,secs))
+        logging.info("\nReading Assets (points) data")
+        df_ast= read_assets(conn)
+        
+        logging.info("\nReading Trails (line) data")
+        gdf_trl= read_trails(conn)
+        
+        logging.info("\nTransforming data")
+        #assets
+        latcol = 'gis_latitude'
+        loncol = 'gis_latitude'
+        gdf_ast= process_assets (df_ast, latcol, loncol)
+        
+        
+    except Exception as e:
+        raise Exception(f"Error occurred: {e}")  
+    
+    finally: 
+        pg.disconnect()
+        
+    
+    
+    finish_t = timeit.default_timer() #finish time
+    t_sec = round(finish_t-start_t)
+    mins = int (t_sec/60)
+    secs = int (t_sec%60)
+    print('\nProcessing Completed in {} minutes and {} seconds'.format (mins,secs))
+            
