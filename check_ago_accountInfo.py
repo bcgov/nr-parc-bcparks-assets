799ca63975be7903e9eac447855dc91d7591dd60
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2025-05-29 09:54:42 -0700

    rename file

--- check_ago_accountInfo.py
+++ check_ago_accountInfo.py
@@ -3,8 +3,8 @@ from arcgis.gis import GIS
 
 gis = GIS(
     'https://governmentofbc.maps.arcgis.com', 
-    os.getenv('AGO_USERNAME_ML'), 
-    os.getenv('AGO_PASSWORD_ML'), 
+    os.getenv('AGO_USERNAME_BP'), 
+    os.getenv('AGO_PASSWORD_BCPARKS'), 
     verify_cert=False
 ) 
 
@@ -17,9 +17,21 @@ print(f"Role:             {me.role}")
 print(f"User Type:        {me.userType}")    
 print(f"License Type ID:  {me.userLicenseTypeId}")
 
-'''
+
 privileges = gis.users.me.privileges
 print(f"\n{me.username} Account Privileges:")
 for privilege in sorted(privileges):
     print(f"  {privilege}")
-'''
+
+
+for folder in me.folders:
+    title = folder['title']
+    fid   = folder['id']
+    print(f"Folder: {title}")
+
+    # when we hit AMS Data, list title, type, and owner of each item
+    if title == 'AMS Data':
+        items = me.items(folder=fid, max_items=500)
+        print("  Items in 'AMS Data':")
+        for itm in items:
+            print(f"   • {itm.title} ({itm.type}) – Owner: {itm.owner}")
